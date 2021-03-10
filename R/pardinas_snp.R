37ff719c8aad80eedc86e04ff405b7644e18d63e
eugene100hickey <eugene.hickey@ittdublin.ie>
2021-03-10 15:34:10 +0000

    snp function

---
+++ R/pardinas_snp.R
@@ -0,0 +1,22 @@
+#' Output the list of schizophrenia associated snps from Pardinas (PMID:29483656)
+#'
+#' @return a tibble of schizophreina snps with a column labelled 'snps' and a column names 'genes
+#' @export
+#'
+#' @examples pardinas_snp()
+pardinas_snp <- function() {
+  url <- "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5918692/bin/NIHMS958804-supplement-Supplementary_Table.xlsx"
+  httr::GET(url, httr::write_disk(temp_file <- tempfile(fileext = ".xlsx"))) # downloads the .xlsx file
+  df <- readxl::read_excel(temp_file, sheet = 4, skip = 3) # reads into a dataframe. First six rows of the excel file are just header
+  unlink(temp_file)     # deletes the temporary file
+  pins::pin(x = df, name = "pardinas_snps")
+
+
+  ##################################################################
+  # makes sz_snps, a dataframe with a column of the CLOZUK snps
+  #######################################################################
+
+  all_sz_genes <- df %>% select(`Index SNP (dbSNP b141)`, `Gene(s) tagged`)
+  names(all_sz_genes) <- c("snps", "genes")
+  all_sz_genes
+}
