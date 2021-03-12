9aabac25cf306be01cd5dd1fd065c89267731c22
eugene100hickey <eugene.hickey@ittdublin.ie>
2021-03-12 16:09:13 +0000

    sorted out ABAgenes

---
+++ R/abagenes.R
@@ -0,0 +1,24 @@
+#' Data from the Allen Brain Atlas.
+#'
+#' gene identifiers from ABA
+#'
+#'
+#' \itemize{
+#'   \item hgnc_genes. The SYMBOL of each gene.
+#'   \item entrezgene. entrezgene id.
+#'   \item ensembl_gene_id. ensembl identifier.
+#' }
+#'
+#' @docType data
+#'
+#' @usage data(abagenes)
+#'
+#' @format An object of class \code{"data.frame"}.
+#'
+#' @keywords datasets
+#'
+#' @source  Allen Brain Atlas
+#' @examples
+#' data(abagenes)
+#' abagenes
+"abagenes"
